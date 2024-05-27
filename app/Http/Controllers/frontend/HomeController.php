961c27187472cc4c51126a55aede2b493ab57421
ledanthuan_web1 <ledanthuan2709@gmail.com>
2024-05-27 13:00:41 +0700

    buoi2

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $list = DB::table('menu')->get();
+        foreach($list as $row)
+        {
+            echo "<h1>".$row->name."</h1>";
+        }
         return view('frontend.home');
     }
 }
---
+++ database/migrations/2024_05_20_070147_create_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ledanthuan_order', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->string('note');
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ledanthuan_order');
+    }
+};
